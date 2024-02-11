ebcb52482efa69b6c6ac1bd51e60b0be3a5d7687
lNewED2 <NewHaZao.825411@gmail.com>
2024-02-11 22:07:03 +0700

    Footer

--- client/src/layout/Main.jsx
+++ client/src/layout/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../conponents/NavBar'
+import Footer from '../conponents/Footer'
 
 const Main = () => {
   return (
     <div>
         <Navbar />
         <Outlet />
+        <Footer />
         <footer>Footer</footer>
     </div>
   )
